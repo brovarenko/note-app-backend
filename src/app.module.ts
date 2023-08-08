7b05ead708e4a5de5e385bd800836802f5ed0043
vladyslav-brovarenko <brovwg@gmail.com>
2023-08-08 06:46:54 +0300

    added sequelize module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { NotesModule } from './notes/notes.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(
-      'mongodb+srv://brovwg:12345@cluster0.f3yejut.mongodb.net/',
-    ),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'mydb',
+      models: [],
+    }),
     NotesModule,
   ],
 })
